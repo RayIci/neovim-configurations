66cc1e5fa0f5c7ca30b333aa0df0e00d1b2867da
Alex Valle <alexvalle75@gmail.com>
2024-10-29 12:41:20 +0100

    Update indent-blankline.lua

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,25 +1,28 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-        indent = {
-            char = "▏",
-        },
-        scope = {
-            show_start = false,
-            show_end = false,
-            show_exact_scope = false,
-        },
-        exclude = {
-            filetypes = {
-                "help",
-                "startify",
-                "dashboard",
-                "packer",
-                "neogitstatus",
-                "NvimTree",
-                "Trouble",
-            },
-        },
-    },
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {
+		indent = {
+			char = "┊", -- https://www.compart.com/en/unicode/U+250A
+			-- char = "⠇",
+			-- char = "¦",
+			-- char = "▏",
+		},
+		scope = {
+			show_start = false,
+			show_end = false,
+			show_exact_scope = false,
+		},
+		exclude = {
+			filetypes = {
+				"help",
+				"startify",
+				"dashboard",
+				"packer",
+				"neogitstatus",
+				"NvimTree",
+				"Trouble",
+			},
+		},
+	},
 }
