8efeaef9d177b4ce85707e86f28f0c464c61519f
Alex Valle <alexvalle75@gmail.com>
2024-10-29 14:17:44 +0100

    Added lazygit plugin

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,19 @@
+-- To install lazygit see the following link: https://github.com/jesseduffield/lazygit?tab=readme-ov-file
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+  },
+}
