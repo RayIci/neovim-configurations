ecb66e26ccdbd6c57abf6784a46ef98e60b73ea9
Alex Valle <alexvalle75@gmail.com>
2024-10-29 20:14:03 +0100

    Transparent tokyonight + fix transparent float

--- lua/themes/tokyo-night.lua
+++ lua/themes/tokyo-night.lua
@@ -6,36 +6,42 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			style = "storm", -- moon, storm, night, day
-			on_highlights = function(hl, c)
-				local prompt = "#2d3149"
-				hl.TelescopeNormal = {
-					bg = c.bg_dark,
-					fg = c.fg_dark,
-				}
-				hl.TelescopeBorder = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-				hl.TelescopePromptNormal = {
-					bg = prompt,
-				}
-				hl.TelescopePromptBorder = {
-					bg = prompt,
-					fg = prompt,
-				}
-				hl.TelescopePromptTitle = {
-					bg = prompt,
-					fg = prompt,
-				}
-				hl.TelescopePreviewTitle = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-				hl.TelescopeResultsTitle = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-			end,
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+
+			-- on_highlights = function(hl, c)
+			-- 	local prompt = "#2d3149"
+			-- 	hl.TelescopeNormal = {
+			-- 		bg = c.bg_dark,
+			-- 		fg = c.fg_dark,
+			-- 	}
+			-- 	hl.TelescopeBorder = {
+			-- 		bg = c.bg_dark,
+			-- 		fg = c.bg_dark,
+			-- 	}
+			-- 	hl.TelescopePromptNormal = {
+			-- 		bg = prompt,
+			-- 	}
+			-- 	hl.TelescopePromptBorder = {
+			-- 		bg = prompt,
+			-- 		fg = prompt,
+			-- 	}
+			-- 	hl.TelescopePromptTitle = {
+			-- 		bg = prompt,
+			-- 		fg = prompt,
+			-- 	}
+			-- 	hl.TelescopePreviewTitle = {
+			-- 		bg = c.bg_dark,
+			-- 		fg = c.bg_dark,
+			-- 	}
+			-- 	hl.TelescopeResultsTitle = {
+			-- 		bg = c.bg_dark,
+			-- 		fg = c.bg_dark,
+			-- 	}
+			-- end,
 		})
 
 		vim.cmd.colorscheme("tokyonight")
