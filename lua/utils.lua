497a22714fa7f485659d34a3d248ced61aff2c88
Alex Valle <alexvalle75@gmail.com>
2024-12-11 20:34:14 +0100

    Adde util to toggle diagnostic list

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,23 @@
 local utils = {}
 
+-- GENERAL UTILS
 function utils.mapkey(mode, key, functionalty, opts)
     vim.keymap.set(mode, key, functionalty, vim.tbl_deep_extend("force", { noremap = true, silent = true }, opts))
+end
+
+-- DIAGNOSTIC UTILS
+function utils.toggle_diagnostic_list()
+    local is_open = false
+    for _, win in pairs(vim.fn.getwininfo()) do
+        if win.loclist == 1 then
+            is_open = true
+        end
+    end
+    if is_open then
+        vim.cmd("lclose")
+    else
+        vim.diagnostic.setloclist()
+    end
 end
 
 return utils
