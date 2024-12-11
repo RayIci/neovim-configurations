64112633d79e4bbf129433cac0a66fed14ade175
Alex Valle <alexvalle75@gmail.com>
2024-12-11 20:32:10 +0100

    Added warning, error, etc... keymap diagnostic

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,124 +1,132 @@
+local utils = require("utils")
+local map = utils.mapkey
+
 -- Leader set
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- For conciseness
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set("n", "J", ":m .+1<CR>==", vim.tbl_extend("force", opts, { desc = "Move Line Up" }))
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "Move Line Up" }))
-vim.keymap.set("n", "K", ":m .-2<CR>==", vim.tbl_extend("force", opts, { desc = "Move Line Down" }))
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "Move Line Down" }))
+map("n", "J", ":m .+1<CR>==", { desc = "Move Line Up" })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line Up" })
+map("n", "K", ":m .-2<CR>==", { desc = "Move Line Down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Line Down" })
 
 -- Formatting
-vim.keymap.set(
-    "n",
-    "<leader>f",
-    "<cmd>lua vim.lsp.buf.format()<CR>",
-    vim.tbl_extend("force", opts, { desc = "[F]ormat Document" })
-)
+map("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<CR>", { desc = "[F]ormat Document" })
 
 -- save file
-vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", vim.tbl_extend("force", opts, { desc = "[S]ave Document" }))
+map("n", "<C-s>", "<cmd> w <CR>", { desc = "[S]ave Document" })
 
 -- save file without auto-formatting
-vim.keymap.set(
-    "n",
-    "<leader>sn",
-    "<cmd>noautocmd w <CR>",
-    vim.tbl_extend("force", opts, { desc = "[S]ave [N]o Format" })
-)
+map("n", "<leader>sn", "<cmd>noautocmd w <CR>", { desc = "[S]ave [N]o Format" })
 
 -- delete single character without copying into register
-vim.keymap.set("n", "x", '"_x', vim.tbl_extend("force", opts, { desc = "Delete Signle Char (no buff save)" }))
+map("n", "x", '"_x', { desc = "Delete Signle Char (no buff save)" })
 
 -- Vertical scroll and center
-vim.keymap.set("n", "<C-d>", "<C-d>zz", vim.tbl_extend("force", opts, { desc = "Scroll Down" }))
-vim.keymap.set("n", "<C-u>", "<C-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll Up" }))
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll Down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll Up" })
 
 -- Find and center
-vim.keymap.set("n", "n", "nzzzv", vim.tbl_extend("force", opts, { desc = "Find Next" }))
-vim.keymap.set("n", "N", "Nzzzv", vim.tbl_extend("force", opts, { desc = "Find Previous" }))
+map("n", "n", "nzzzv", { desc = "Find Next" })
+map("n", "N", "Nzzzv", { desc = "Find Previous" })
 
 -- Resize with arrows
-vim.keymap.set("n", "<Up>", ":resize -2<CR>", vim.tbl_extend("force", opts, { desc = "Resize Horizontal (-)" }))
-vim.keymap.set("n", "<Down>", ":resize +2<CR>", vim.tbl_extend("force", opts, { desc = "Resize Horizontal (+)" }))
-vim.keymap.set(
-    "n",
-    "<Left>",
-    ":vertical resize -2<CR>",
-    vim.tbl_extend("force", opts, { desc = "Resize Vertical (+)" })
-)
-vim.keymap.set(
-    "n",
-    "<Right>",
-    ":vertical resize +2<CR>",
-    vim.tbl_extend("force", opts, { desc = "Resize Vertinal (-)" })
-)
+map("n", "<Up>", ":resize -2<CR>", { desc = "Resize Horizontal (-)" })
+map("n", "<Down>", ":resize +2<CR>", { desc = "Resize Horizontal (+)" })
+map("n", "<Left>", ":vertical resize -2<CR>", { desc = "Resize Vertical (+)" })
+map("n", "<Right>", ":vertical resize +2<CR>", { desc = "Resize Vertinal (-)" })
 
 -- Buffers
-vim.keymap.set(
-    "n",
-    "<C-Left>",
-    ":BufferLineMovePrev<CR>",
-    vim.tbl_extend("force", opts, { desc = "Buffer Move Previous" })
-)
-vim.keymap.set(
-    "n",
-    "<C-Right>",
-    ":BufferLineMoveNext<CR>",
-    vim.tbl_extend("force", opts, { desc = "Buffer Move Next" })
-)
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", vim.tbl_extend("force", opts, { desc = "Buffer Next" }))
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", vim.tbl_extend("force", opts, { desc = "Buffer Previous" }))
-vim.keymap.set("n", "<leader>xw", ":Bdelete<CR>", vim.tbl_extend("force", opts, { desc = "Buffer Delete [W]indow" })) -- close buffer
-vim.keymap.set(
-    "n",
-    "<leader>xa",
-    ":bufdo :Bdelete<CR>",
-    vim.tbl_extend("force", opts, { desc = "Buffer Delete [A]ll" })
-) -- close buffer
-vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", vim.tbl_extend("force", opts, { desc = "Buffer New" })) -- new buffer
+map("n", "<C-Left>", ":BufferLineMovePrev<CR>", { desc = "Buffer Move Previous" })
+map("n", "<C-Right>", ":BufferLineMoveNext<CR>", { desc = "Buffer Move Next" })
+map("n", "<Tab>", ":bnext<CR>", { desc = "Buffer Next" })
+map("n", "<S-Tab>", ":bprevious<CR>", { desc = "Buffer Previous" })
+map("n", "<leader>xw", ":Bdelete<CR>", { desc = "Buffer Delete [W]indow" }) -- close buffer
+map("n", "<leader>xa", ":bufdo :Bdelete<CR>", { desc = "Buffer Delete [A]ll" }) -- close buffer
+map("n", "<leader>b", "<cmd> enew <CR>", { desc = "Buffer New" }) -- new buffer
 
 -- Window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", vim.tbl_extend("force", opts, { desc = "Split Window [V]ertically" })) -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", vim.tbl_extend("force", opts, { desc = "Split Window [H]rizontally" })) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", vim.tbl_extend("force", opts, { desc = "Make [S]plit [E]qual Size" })) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>xs", ":close<CR>", vim.tbl_extend("force", opts, { desc = "Split Close" })) -- close current split window
+map("n", "<leader>v", "<C-w>v", { desc = "Split Window [V]ertically" }) -- split window vertically
+map("n", "<leader>h", "<C-w>s", { desc = "Split Window [H]rizontally" }) -- split window horizontally
+map("n", "<leader>se", "<C-w>=", { desc = "Make [S]plit [E]qual Size" }) -- make split windows equal width & height
+map("n", "<leader>xs", ":close<CR>", { desc = "Split Close" }) -- close current split window
 
 -- Navigate between splits
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", vim.tbl_extend("force", opts, { desc = "Split Navigate Up" }))
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", vim.tbl_extend("force", opts, { desc = "Split Navigate Down" }))
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", vim.tbl_extend("force", opts, { desc = "Split Navigate Left" }))
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", vim.tbl_extend("force", opts, { desc = "Split Navigate Right" }))
+map("n", "<C-k>", ":wincmd k<CR>", { desc = "Split Navigate Up" })
+map("n", "<C-j>", ":wincmd j<CR>", { desc = "Split Navigate Down" })
+map("n", "<C-h>", ":wincmd h<CR>", { desc = "Split Navigate Left" })
+map("n", "<C-l>", ":wincmd l<CR>", { desc = "Split Navigate Right" })
 
 -- Tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", vim.tbl_extend("force", opts, { desc = "[T]ab [O]pen New" })) -- open new tab
-vim.keymap.set("n", "<leader>xt", ":tabclose<CR>", vim.tbl_extend("force", opts, { desc = "[T]ab Close" })) -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", vim.tbl_extend("force", opts, { desc = "[T]ab [N]ext" })) --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", vim.tbl_extend("force", opts, { desc = "[T]ab [P]revious" })) --  go to previous tab
-vim.keymap.set("n", "<leader>tss", ":tab split<CR>", vim.tbl_extend("force", opts, { desc = "[T]ab [S]plit" })) -- split tab
+map("n", "<leader>to", ":tabnew<CR>", { desc = "[T]ab [O]pen New" }) -- open new tab
+map("n", "<leader>xt", ":tabclose<CR>", { desc = "[T]ab Close" }) -- close current tab
+map("n", "<leader>tn", ":tabn<CR>", { desc = "[T]ab [N]ext" }) --  go to next tab
+map("n", "<leader>tp", ":tabp<CR>", { desc = "[T]ab [P]revious" }) --  go to previous tab
+map("n", "<leader>tss", ":tab split<CR>", { desc = "[T]ab [S]plit" }) -- split tab
 
 -- Toggle line wrapping
-vim.keymap.set(
-    "n",
-    "<leader>tlw",
-    "<cmd>set wrap!<CR>",
-    vim.tbl_extend("force", opts, { desc = "[T]oggle [L]ine [W]rap" })
-)
+map("n", "<leader>tlw", "<cmd>set wrap!<CR>", { desc = "[T]oggle [L]ine [W]rap" })
 
 -- Stay in indent mode
-vim.keymap.set("v", "<", "<gv", vim.tbl_extend("force", opts, { desc = "Indent Left" }))
-vim.keymap.set("v", ">", ">gv", vim.tbl_extend("force", opts, { desc = "Indent Right" }))
+map("v", "<", "<gv", { desc = "Indent Left" })
+map("v", ">", ">gv", { desc = "Indent Right" })
 
 -- Keep last yanked when pasting
-vim.keymap.set("v", "p", '"_dP', vim.tbl_extend("force", opts, { desc = "Paste" }))
+map("v", "p", '"_dP', { desc = "Paste" })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>dm", vim.diagnostic.open_float, { desc = "Open floating [D]iagnostic [M]essage" })
-vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open [D]iagnostics [L]ist" })
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+map(
+    "n",
+    "[e",
+    "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>",
+    { desc = "Prev [E]rror Diagnostic" }
+)
+map(
+    "n",
+    "]e",
+    "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>",
+    { desc = "Next [E]rror Diagnostic" }
+)
+map(
+    "n",
+    "[w",
+    "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.WARN})<CR>",
+    { desc = "Prev [W]arning Diagnostic" }
+)
+map(
+    "n",
+    "]w",
+    "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.WARN})<CR>",
+    { desc = "Next [W]arning Diagnostic" }
+)
+map(
+    "n",
+    "[i",
+    "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.INFO})<CR>",
+    { desc = "Prev [I]nfo Diagnostic" }
+)
+map(
+    "n",
+    "]i",
+    "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.INFO})<CR>",
+    { desc = "Next [I]nfo Diagnostic" }
+)
+map(
+    "n",
+    "[h",
+    "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.HINT})<CR>",
+    { desc = "Prev [H]int Diagnostic" }
+)
+map(
+    "n",
+    "]h",
+    "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.HINT})<CR>",
+    { desc = "Next [H]int Diagnostic" }
+)
+map("n", "<leader>dw", vim.diagnostic.open_float, { desc = "Open floating [D]iagnostic [M]essage" })
+map("n", "<leader>dl", utils.toggle_diagnostic_list, { desc = "Open [D]iagnostics [L]ist" })
