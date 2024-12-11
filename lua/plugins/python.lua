6c37f21110e2bf87f138bcd5ce3776dc09e8c1be
Alex Valle <alexvalle75@gmail.com>
2024-12-11 13:19:15 +0100

    Added python venv selector

---
+++ lua/plugins/python.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        -- Virtual environment selector
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap",
+            "mfussenegger/nvim-dap-python", --optional
+            { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        },
+        config = function()
+            require("venv-selector").setup({
+                auto_refresh = true,
+                search = true,
+            })
+        end,
+        branch = "regexp",
+        event = "VeryLazy",
+        keys = {
+            { ",vs", "<cmd>VenvSelect<cr>" },
+            { ",vc", "<cmd>VenvSelectCached<cr>" },
+        },
+    },
+}
