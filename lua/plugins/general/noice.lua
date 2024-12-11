d015f03db18422deef156a5fd86365f21cd3aa1e
Alex Valle <alexvalle75@gmail.com>
2024-12-11 20:33:59 +0100

    Added border to signature and other floating windows with noice plugin

--- lua/plugins/general/noice.lua
+++ lua/plugins/general/noice.lua
@@ -22,7 +22,16 @@ return {
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
+                lsp_doc_border = true, -- add a border to hover docs and signature help
+            },
+            signature = {
+                enabled = true,
+                auto_open = {
+                    enabled = true,
+                    trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+                    luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+                },
+                view = nil, -- when nil, use defaults from documentation
             },
         })
     end,
