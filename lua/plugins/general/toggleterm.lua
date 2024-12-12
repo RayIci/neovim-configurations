ac77e69750575fe89feee75c0c89bf678fe27e81
Alex Valle <alexvalle75@gmail.com>
2024-12-12 22:58:04 +0100

    Small changes to toggleterm plugin

--- lua/plugins/general/toggleterm.lua
+++ lua/plugins/general/toggleterm.lua
@@ -1,3 +1,5 @@
+local map = require("utils").mapkey
+
 return {
     "akinsho/toggleterm.nvim",
     config = function()
@@ -8,11 +10,11 @@ return {
                 elseif term.direction == "vertical" then
                     return vim.o.columns * 0.4
                 end
-            end,                      -- Default size for horizontal terminals
-            open_mapping = [[<C-\>]], -- Default toggle key
-            direction = "horizontal", -- Default direction
-            close_on_exit = true,     -- Automatically close terminals on process exit
-            shell = vim.o.shell,      -- Use the default shell
+            end,
+            open_mapping = [[<C-\>]],
+            direction = "horizontal",
+            close_on_exit = true,
+            shell = vim.o.shell,
             shading_factor = 2,
             shade_terminals = true,
             start_in_insert = true,
@@ -22,12 +24,12 @@ return {
 
         function _G.set_terminal_keymaps()
             local opts = { buffer = 0 }
-            vim.keymap.set("t", "<C-z>", [[<C-\><C-n>]], opts)
-            vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-            vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-            vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-            vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-            vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+            map("t", "<C-z>", [[<C-\><C-n>]], opts)
+            map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+            map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+            map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+            map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+            map("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
         end
 
         -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -36,14 +38,14 @@ return {
         local Terminal = require("toggleterm.terminal").Terminal
 
         -- Keymaps for toggling terminals
-        vim.keymap.set("n", "<leader>tv", function()
+        map("n", "<leader>tv", function()
             local vterm = Terminal:new({ direction = "vertical" })
             vterm:toggle()
-        end, { noremap = true, silent = true, desc = "[T]erminal [V]ertical" })
+        end, { desc = "[T]erminal [V]ertical" })
 
-        vim.keymap.set("n", "<leader>th", function()
+        map("n", "<leader>th", function()
             local hterm = Terminal:new({ direction = "horizontal" })
             hterm:toggle()
-        end, { noremap = true, silent = true, desc = "[T]erminal [H]orizontal" })
+        end, { desc = "[T]erminal [H]orizontal" })
     end,
 }
