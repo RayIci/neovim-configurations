abd3965f31748db7185716351729a9945f1d02f4
Alex Valle <alexvalle75@gmail.com>
2024-12-14 13:16:18 +0100

    Added diagnostic sort

--- lua/plugins/code/none-ls.lua
+++ lua/plugins/code/none-ls.lua
@@ -1,9 +1,9 @@
 local ensure_installed = {
-    "prettier", -- ts/js formatter
-    "stylua", -- lua formatter
-    "eslint_d", -- ts/js linter
-    "shfmt", -- Shell formatter
-    "black", -- Python formatter
+    "prettier",  -- ts/js formatter
+    "stylua",    -- lua formatter
+    "eslint_d",  -- ts/js linter
+    "shfmt",     -- Shell formatter
+    "black",     -- Python formatter
     "csharpier", -- C# formatter
     "rustywind", -- tailwind classes organizer
 }
@@ -31,10 +31,11 @@ return {
     },
     config = function()
         vim.diagnostic.config({
-            virtual_text = false, -- Disable virtual text (error shown in the editor)
-            signs = true, -- Show error, warning, etc... signs
-            underline = true, -- Show the line under an error, warning, etc ...
+            virtual_text = false,    -- Disable virtual text (error shown in the editor)
+            signs = true,            -- Show error, warning, etc... signs
+            underline = true,        -- Show the line under an error, warning, etc ...
             update_in_insert = true, -- Update the errors, warnings also on insert
+            severity_sort = true,
             float = {
                 source = "always",
                 border = "rounded",
@@ -46,8 +47,18 @@ return {
         vim.cmd.highlight("DiagnosticUnderlineInfo guisp=#00FFFF gui=undercurl")
         vim.cmd.highlight("DiagnosticUnderlineHint guisp=#00FF00 gui=undercurl")
 
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = function()
+                vim.cmd("highlight DiagnosticError guisp=#FF0000 gui=undercurl")
+                vim.cmd("highlight DiagnosticWarn guisp=#FFA500 gui=undercurl")
+                vim.cmd("highlight DiagnosticInfo guisp=#00FFFF gui=undercurl")
+                vim.cmd("highlight DiagnosticHint guisp=#00FF00 gui=undercurl")
+            end,
+        })
+
         local null_ls = require("null-ls")
-        local formatting = null_ls.builtins.formatting -- to setup formatters
+        local formatting = null_ls.builtins.formatting   -- to setup formatters
         local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
         -- Formatters & linters for mason to install
