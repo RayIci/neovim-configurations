876091f68eb2b8f1c59a322b20306fbc3d792f6d
Alex Valle <alexvalle75@gmail.com>
2024-12-16 15:03:59 +0100

    Updated trouble keymaps with which-key

--- lua/plugins/ui/trobule.lua
+++ lua/plugins/ui/trobule.lua
@@ -1,40 +1,46 @@
 -- Trouble is a plugin that makes it easy to see and navigate diagnostics, references, and other errors in a buffer.
 -- https://github.com/folke/trouble.nvim
 
+local keymaps = function()
+    require("which-key").add({
+        { "<leader>dd",  group = "Diagnostics" },
+        { "<leader>ddw", "<cmd>Trouble diagnostics toggle<cr>", desc = "Window (diagnostic) Toggle" },
+        {
+            "<leader>ddb",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Window (diagnostic) Buffer Toggle",
+        },
+    })
+end
+
 return {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
+    config = function()
+        require("trouble").setup({})
+        keymaps()
+    end,
     keys = {
-        {
-            "<leader>dk",
-            "<cmd>Trouble diagnostics toggle<cr>",
-            desc = "[D]iagnostic Window (Trouble)",
-        },
-        {
-            "<leader>dkb",
-            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-            desc = "[D]iagnostic Window [B]uffer  (Trouble)",
-        },
         {
             "<leader>sl",
             "<cmd>Trouble symbols toggle focus=false<cr>",
-            desc = "[S]ymbols [L]ist (Trouble)",
+            desc = "Symbols List",
         },
         {
             "<leader>lw",
             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "[L]LSP [W]indow, Definitions/references/... (Trouble)",
+            desc = "LSP Window, Definitions/references/... (Trouble)",
         },
         {
             "<leader>ll",
             "<cmd>Trouble loclist toggle<cr>",
-            desc = "[L]ocation [L]ist (Trouble)",
+            desc = "Location List",
         },
         {
             "<leader>ql",
             "<cmd>Trouble qflist toggle<cr>",
-            desc = "[Q]uickfix [L]ist (Trouble)",
+            desc = "Quickfix List",
         },
     },
 }
