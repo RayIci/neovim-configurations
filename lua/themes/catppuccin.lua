7e90c362826e14bd75401147c28da08773501071
Alex Valle <alexvalle75@gmail.com>
2024-12-16 14:29:16 +0100

    Changed catppuccin theme transparency toggle keymap

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -5,28 +5,28 @@ return {
     lazy = false,
     config = function()
         -- Keymap that for transparency toggle
-        vim.keymap.set("n", "<leader>tt", function()
+        vim.keymap.set("n", "<leader>tv", function()
             local cat = require("catppuccin")
             cat.options.transparent_background = not cat.options.transparent_background
             cat.compile()
             vim.cmd.colorscheme(vim.g.colors_name)
-        end, { desc = "[T]heme Transparency [T]oggle" })
+        end, { desc = "Theme Transparency Toggle" })
 
         require("catppuccin").setup({
-            flavour = "frappe", -- latte, frappe, macchiato, mocha
+            flavour = "frappe",            -- latte, frappe, macchiato, mocha
             transparent_background = true, -- disables setting the background color.
-            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-            term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+            show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+            term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
-                enabled = false, -- dims the background color of inactive window
+                enabled = false,           -- dims the background color of inactive window
                 shade = "dark",
-                percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                percentage = 0.15,         -- percentage of the shade to apply to the inactive window
             },
-            no_italic = false, -- Force no italic
-            no_bold = false, -- Force no bold
-            no_underline = false, -- Force no underline
-            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-                comments = { "italic" }, -- Change the style of comments
+            no_italic = false,             -- Force no italic
+            no_bold = false,               -- Force no bold
+            no_underline = false,          -- Force no underline
+            styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" },   -- Change the style of comments
                 conditionals = { "italic" },
             },
             color_overrides = {},
