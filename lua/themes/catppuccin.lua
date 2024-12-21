350e2e1e8c8a398f85a6d41d939aebd7fae1ccb7
Alex Valle <alexvalle75@gmail.com>
2024-12-21 13:35:05 +0100

    Update catppuccin theme

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -13,14 +13,14 @@ return {
         end, { desc = "Theme Transparency Toggle" })
 
         require("catppuccin").setup({
-            flavour = "frappe",            -- latte, frappe, macchiato, mocha
+            flavour = "mocha",             -- latte, frappe, macchiato, mocha
             transparent_background = true, -- disables setting the background color.
             show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
             term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
                 enabled = false,           -- dims the background color of inactive window
                 shade = "dark",
-                percentage = 0.15,         -- percentage of the shade to apply to the inactive window
+                percentage = 0.35,         -- percentage of the shade to apply to the inactive window
             },
             no_italic = false,             -- Force no italic
             no_bold = false,               -- Force no bold
@@ -29,7 +29,7 @@ return {
                 comments = { "italic" },   -- Change the style of comments
                 conditionals = { "italic" },
             },
-            color_overrides = {},
+            -- color_overrides = {},
             custom_highlights = function(colors)
                 return {
                     DiagnosticError = { sp = colors.red, style = { "undercurl" } },
