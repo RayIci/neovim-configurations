e032eab0709f4b1e3ebc4f9c932c5a0c7d56e4a4
Alex Valle <alexvalle75@gmail.com>
2025-01-05 02:39:19 +0100

    Changed alpha

--- lua/plugins/ui/alpha.lua
+++ lua/plugins/ui/alpha.lua
@@ -1,3 +1,66 @@
+-- https://patorjk.com/software/taag/#p=display&v=0&f=ANSI%20Shadow&t=Tomo
+
+local icons = {
+    neovim = {
+        [[                                                    ]],
+        [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+        [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+        [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+        [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+        [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+        [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+        [[                                                    ]],
+    },
+    pika = {
+
+        [[          ▀████▀▄▄              ▄█ ]],
+        [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+        [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+        [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+        [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+        [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+        [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+        [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+        [[   █   █  █      ▄▄           ▄▀   ]],
+    },
+    strange = {
+        "            :h-                                  Nhy`               ",
+        "           -mh.                           h.    `Ndho               ",
+        "           hmh+                          oNm.   oNdhh               ",
+        "          `Nmhd`                        /NNmd  /NNhhd               ",
+        "          -NNhhy                      `hMNmmm`+NNdhhh               ",
+        "          .NNmhhs              ```....`..-:/./mNdhhh+               ",
+        "           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
+        "           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
+        "      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
+        " .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
+        " h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
+        " hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
+        " /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
+        "  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
+        "   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
+        "     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
+        "       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
+        "       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
+        "       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
+        "       //+++//++++++////+++///::--                 .::::-------::   ",
+        "       :/++++///////////++++//////.                -:/:----::../-   ",
+        "       -/++++//++///+//////////////               .::::---:::-.+`   ",
+        "       `////////////////////////////:.            --::-----...-/    ",
+        "        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
+        "         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
+        "           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
+        "            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
+        "           s-`::--:::------:////----:---.-:::...-.....`./:          ",
+        "          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
+        "         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
+        "        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
+        "                        .-:mNdhh:.......--::::-`                    ",
+        "                           yNh/..------..`                          ",
+        "                                                                    ",
+    },
+}
+
 return {
     "goolord/alpha-nvim",
     dependencies = {
@@ -5,20 +68,35 @@ return {
     },
 
     config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.startify")
+        local status_ok, alpha = pcall(require, "alpha")
+        if not status_ok then
+            return
+        end
 
-        dashboard.section.header.val = {
-            [[                                                    ]],
-            [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
-            [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
-            [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
-            [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
-            [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
-            [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
-            [[                                                    ]],
+        local dashboard = require("alpha.themes.dashboard")
+        dashboard.section.header.val = icons.strange
+
+        dashboard.section.buttons.val = {
+            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+            dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+            dashboard.button("m", "  BookMarks", ":Telescope marks <CR>"),
+            dashboard.button("q", "󰩈  Quit Neovim", ":qa<CR>"),
         }
 
+        local function footer()
+            return "Don't Stop Until You are Proud..."
+        end
+
+        dashboard.section.footer.val = footer()
+
+        dashboard.section.footer.opts.hl = "Type"
+        dashboard.section.header.opts.hl = "Include"
+        dashboard.section.buttons.opts.hl = "Keyword"
+
+        dashboard.opts.opts.noautocmd = true
         alpha.setup(dashboard.opts)
     end,
 }
