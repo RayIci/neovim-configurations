6e3e26f558a0fbcee667f93ff393446d3f48bac3
Alex Valle <alexvalle75@gmail.com>
2025-01-05 01:54:00 +0100

    Changed folding

--- lua/plugins/ui/folding.lua
+++ lua/plugins/ui/folding.lua
@@ -24,33 +24,56 @@ local keymaps = function()
 end
 
 return {
-    "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async" },
-    config = function()
-        -- vim.o.foldcolumn = "1" -- '0' is not bad
-        vim.o.foldcolumn = "auto:9"
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async" },
+        config = function()
+            -- vim.o.foldcolumn = "auto:9"
+            vim.o.foldcolumn = "1" -- '0' is not bad
+            vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
 
-        -- Option 3: treesitter as a main provider instead
-        -- (Note: the `nvim-treesitter` plugin is *not* needed.)
-        -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
-        -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
-        require("ufo").setup({
-            provider_selector = function(bufnr, filetype, buftype)
-                return { "treesitter", "indent" }
-            end,
-        })
+            -- Option 3: treesitter as a main provider instead
+            -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+            -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+            -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+            require("ufo").setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return { "treesitter", "indent" }
+                end,
+            })
 
-        vim.opt.fillchars = {
-            fold = " ",
-            foldopen = "▼",
-            foldclose = "▶",
-            foldsep = "│",
-            vert = "│",
-        }
+            vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+            -- vim.opt.fillchars = {
+            --     fold = " ",
+            --     foldopen = "▼",
+            --     foldclose = "▶",
+            --     foldsep = "│",
+            --     vert = "│",
+            -- }
 
-        keymaps()
-    end,
+            keymaps()
+        end,
+    },
+    {
+        "luukvbaal/statuscol.nvim",
+        opts = function()
+            local builtin = require("statuscol.builtin")
+            return {
+                setopt = true,
+                -- override the default list of segments with:
+                -- number-less fold indicator, then signs, then line number & separator
+                segments = {
+                    { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+                    { text = { "%s" },             click = "v:lua.ScSa" },
+                    {
+                        text = { builtin.lnumfunc, " " },
+                        condition = { true, builtin.not_empty },
+                        click = "v:lua.ScLa",
+                    },
+                },
+            }
+        end,
+    },
 }
